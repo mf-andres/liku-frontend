c8e1d0b386015b11fc91f6b4206e885f09944be1
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2021-12-08 22:17:50 +0100

    feat: the add gift view goes to the gifts view after the user fills the form correctly and presses the add gift button

--- cypress/integration/add-gift.spec.js
+++ cypress/integration/add-gift.spec.js
@@ -12,4 +12,10 @@ context("The add gift view", () => {
     cy.get("#add-gift-form");
     cy.get("#add-gift-button");
   });
+
+  it("goes to the gifts view after the user fills the form correctly and presses the add gift button", () => {
+    cy.goToAddGift();
+    cy.get("#description-input").type("mochila");
+    cy.get("#add-gift-button").click();
+  });
 });
--- src/app/add-gift/add-gift.component.ts
+++ src/app/add-gift/add-gift.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-gift',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-gift.component.css'],
 })
 export class AddGiftComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onAddGift(): void {
+    this.router.navigate(['gifts']);
+  }
 }
