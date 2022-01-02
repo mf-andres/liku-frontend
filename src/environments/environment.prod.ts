b2934ded89f0b452b5d02e3d3568cf6b9a1b3e96
Andres Munhoz Fernandez <munozfernandezandres@gmail.com>
2022-01-02 13:08:01 +0100

    feat: add configuration for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true,
+  debuggingPresentation: false,
+  birthdaysURL: 'https://liku-b.herokuapp.com/birthdays',
+  birthdayURL: 'https://liku-b.herokuapp.com/birthday',
+  giftsURL: 'https://liku-b.herokuapp.com/gifts',
+  giftedGifts: 'https://liku-b.herokuapp.com/gifted-gifts',
+  giftURL: 'https://liku-b.herokuapp.com/gift',
 };
